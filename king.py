816d5bfff2f4f464c31a15db786a765f7cb308e3
Mafiyahunter <110088114+Mafiyahunter@users.noreply.github.com>
2022-12-31 20:35:20 +0530

    Create king.py
    git clone https://github.com/Mafiyahunter/king.git
    cd king
    python king.py

---
+++ king.py
@@ -0,0 +1,23 @@
+import time 
+from random import randint
+
+for i in range(1,45):
+      print(" ")
+      
+      s= " "
+      
+      
+for i in range(1,10000):
+      count= range(1,100)
+      while (count >0):
+      	s += " " 
+      count-=1
+      
+      
+      if(1%10==0):
+      	print(s+ "happy new year")
+      else:
+      	print(s+"*")
+      
+      s= " "
+      time.sleep(0.3)
